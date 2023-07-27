0e4b3d32e73059c6ebf6d439778ada5a6720a3dc
Maria Kudryashova <kudrmar@list.ru>
2023-07-27 21:02:37 +0300

    Третие задание

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,14 @@
 function compareArrays(arr1, arr2) {
-  
+    let Equal = arr1.length === arr2.length && arr1.every((value, index) => value === arr2[index]);
+  return Equal;
 }
 
+
 function getUsersNamesInAgeRange(users, gender) {
-  
-}
+    const initialValue = 0;
+    let accfilter = users.filter(user => user.gender === gender);
+    result = (accfilter.length === 0) ? 0 : accfilter.reduce((accumulator, currentValue) => accumulator + currentValue.age, initialValue)
+        /accfilter.length  ;
+    return result;
+ }
+ 
