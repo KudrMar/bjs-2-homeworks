09fc3f0e2ed8c07772ab7065949635b2c1904999
Maria Kudryashova <kudrmar@list.ru>
2023-08-01 01:59:34 +0300

    Четвёртое задание

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,29 @@
 function Student(name, gender, age) {
-  
+	this.name = name;
+	this.gender = gender;
+	this.age = age;
+	this.marks = [];
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  
+Student.prototype.setSubject = function(subjectName) {
+	this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.addMarks = function(...marks) {
+	if (this.hasOwnProperty("marks")) {
+		this.marks = [...this.marks, ...marks];
+	} else return 0;
 }
 
-Student.prototype.getAverage = function () {
-  
+Student.prototype.getAverage = function() {
+	if (this.hasOwnProperty("marks")) {
+		let sum = this.marks.reduce((sum, item) => sum + item, 0);
+		return (this.marks.length === 0) ? 0 : sum / this.marks.length;
+	} else return 0;
 }
 
-Student.prototype.exclude = function (reason) {
-  
+Student.prototype.exclude = function(reason) {
+	delete this.marks;
+	delete this.subject;
+	this.excluded = reason;
 }
