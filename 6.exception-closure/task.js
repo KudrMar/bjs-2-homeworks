07d8bc7c250f294a2f0f6cb65530dc4442b6b44c
Maria Kudryashova <kudrmar@list.ru>
2023-08-24 18:20:38 +0300

    Шестое задание

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,75 @@
-﻿
+﻿function validateCount(parseValue) {
+	try {
+		return parseCount(parseValue);
+	} catch (error) {
+		return error;
+	}
+}
+
+function parseCount(parseValue) {
+
+	if (Number.isNaN(Number.parseFloat(parseValue))) {
+		throw new Error('Невалидное значение');
+	}
+	return Number.parseFloat(parseValue);
+}
+
+class Triangle {
+	constructor(...sides) {
+		if (sides.length === 3) {
+			sides.sort((a, b) => a - b);
+			if (sides[0] + sides[1] < sides[2]) {
+				throw new Error('Треугольник с такими сторонами не существует');
+			};
+			this.sides = sides;
+		} else {
+			this.sides = [];
+		}
+	}
+
+	get perimeter() {
+		if (this.sides.length === 3) {
+			const initialValue = 0;
+			let resultPerimeter = this.sides.reduce(
+				(accumulator, currentValue) => accumulator + currentValue,
+				initialValue
+			);
+			return Math.round(resultPerimeter * 1000) / 1000;
+		} else {
+			return "Ошибка! Треугольник не существует"
+		};
+	}
+
+
+	get area() {
+		if (this.sides.length === 3) {
+			const p = this.perimeter / 2;
+			let resultArea = Math.sqrt(p * (p - this.sides[0]) * (p - this.sides[1]) * (p - this.sides[2])).toFixed(3);
+			return Math.round(resultArea * 1000) / 1000;
+		} else {
+			return "Ошибка! Треугольник не существует"
+		};
+	}
+
+}
+
+function getTriangle(...sides) {
+	try {
+		return new Triangle(...sides);
+	} catch {
+		emptyTriangle = new Triangle();
+		return emptyTriangle;
+	}
+}
+
+//class EmptyTriangle {
+//	constructor() {}
+//
+//	get perimeter() {
+//		return "Ошибка! Треугольник не существует";
+//	}
+//
+//	get area() {
+//		return "Ошибка! Треугольник не существует";
+//	}
+//}
